47b3542589e905abf21d51df94ca046bb24422de
Unkn0wn <65486577+unkn0wn-src@users.noreply.github.com>
2020-05-18 17:55:41 +0200

    Add files via upload

--- HashCracker.py
+++ HashCracker.py
@@ -1,28 +1,33 @@
-import hashlib, sys
+import hashlib, sys, codecs
 
 def crack_hash(file, hash, hash_alg):
-    PWD_FILE = open(file)
+    with codecs.open(file, 'r', encoding='utf-8', errors='ignore') as PWD_FILE:
+
+        i = 0
     
-    for line in PWD_FILE.readlines():
+        for line in PWD_FILE.readlines():
 
-        pwd = line.strip('\n')
+            pwd = line.strip('\n')
 
-        if hash_alg == "sha1":
-            pwd_hash = hashlib.sha1(str.encode(pwd))
-        elif hash_alg == "sha256":
-            pwd_hash = hashlib.sha256(str.encode(pwd))
-        elif hash_alg == "sha512":
-            pwd_hash = hashlib.sha512(str.encode(pwd))
-        elif hash_alg == "md5":
-            pwd_hash = hashlib.md5(str.encode(pwd))
-        else:
-            print("[-] Just use one of the given algorithm")
-            break
+            if hash_alg == "sha1":
+                pwd_hash = hashlib.sha1(str.encode(pwd))
+            elif hash_alg == "sha256":
+                pwd_hash = hashlib.sha256(str.encode(pwd))
+            elif hash_alg == "sha512":
+                pwd_hash = hashlib.sha512(str.encode(pwd))
+            elif hash_alg == "md5":
+                pwd_hash = hashlib.md5(str.encode(pwd))
+            else:
+                print("[-] Just use one of the given algorithm")
+                break
 
-        if hash == pwd_hash.hexdigest():
-            print("[!] PASSWORD FOUND")
-            print("[+] The password for your hash is: " + pwd)
-            break
+            if hash == pwd_hash.hexdigest():
+                print("[+] The password for your hash is: " + pwd)
+                break
+
+            i += 1
+
+    print("[!] " + str(i) + " passwords have been hashed and tested")
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
